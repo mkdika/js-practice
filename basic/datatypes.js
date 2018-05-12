2acdad6a422548208504767d9e694c24805d5266
Maikel Chandika <mkdika@gmail.com>
2018-05-12 14:42:05 +0700

    add data type and variable mutation example

---
+++ basic/datatypes.js
@@ -0,0 +1,34 @@
+/*
+    JAVASCRIPT PRIMITIVE DATA TYPES
+    ===============================
+    - Number: Floating point numbers, for decimals and integer
+    - String: Sequence of characters, used for text.
+    - Boolean: Logical data type that can only be true or false
+    - Undefined: Data type of a variable which does not have a value yet
+    - Null: Also means 'non-existent'
+  */
+
+  // string type variable
+  var name = 'John';
+  console.log(name);
+
+  // string type variable
+  var lastName = 'Smith';
+  console.log(name+' '+lastName);
+
+  // numeric type variable
+  var num1 = 123;
+  console.log(num1);
+
+ // numeric type variable
+  var num2 = 12.4578;
+  console.log(num2);
+
+  // boolean type variable
+  var bool1 = true;
+  console.log(bool1); 
+  console.log(bool1);
+
+  // null data type
+  var null1 = null;
+  console.log(null1);
---
+++ basic/variablemutation.js
@@ -0,0 +1,5 @@
+var name = 'John';
+var age = 26;
+
+console.log('Name: '+name+', Age: '+age);
+console.log(age+age);
