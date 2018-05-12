79dce67dce0acd92878719c35750670888ebb119
Maikel Chandika <mkdika@gmail.com>
2018-05-12 17:43:50 +0700

    add some information within loop example

--- basic/loops.js
+++ basic/loops.js
@@ -1,23 +1,28 @@
-// for loop
-for(var i=1;i<11;i++) {
-  console.log(i);
-}
+var names = ['Andy', 'Betty', 'Charlie', 'Danny', 'Edwin'];
+console.log(names);
 
-var names = ['Andy','Betty','Charlie','Danny','Edwin'];
-
+console.log('Use for index:');
 // for loop to travers all array element
-for (var i=0;i<names.length;i++) {
+for (var i = 0; i < names.length; i++) {
   console.log(names[i]);
 }
 
-console.log('');
+console.log('Use in:');
 // for index to travers all array element
 for (i in names) {
   console.log(names[i]);
 }
 
-console.log('');
+console.log('Use of:');
 // for each with of to travers all array element
 for (name of names) {
   console.log(name);
 }
+
+console.log('Use do..while:');
+// use do..while to travers all array element
+var i = 0;
+do {
+  console.log(names[i]);
+  i++;
+}while(i<names.length);
