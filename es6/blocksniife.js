1c26fb3ae2d2eb059dd2e74bfd0e0c08e3db2f64
Maikel Chandika <mkdika@gmail.com>
2018-05-15 16:19:52 +0700

    add es6 string template example

---
+++ es6/blocksniife.js
@@ -0,0 +1,12 @@
+// ES6
+{ // this is block of code
+  const a = 1;
+  let b = 2;
+  var c = 3;  
+}
+console.log(c);
+
+// ES5
+(function() { // use anonymous function
+  var x = 3;
+})();
---
+++ es6/strings.js
@@ -0,0 +1,15 @@
+// ES6 String Template Demo
+
+let firstName = 'John';
+let lastName = 'Smith';
+const yearOfBirth = 1990;
+
+function calcAge(year) {
+  return 2016 - year;
+}
+
+// ES5
+console.log('This is '+firstName+' '+lastName+'. He was born in '+yearOfBirth+'. Today, he is '+calcAge(yearOfBirth)+' years old.');
+
+// ES6
+console.log(`This is ${firstName} ${lastName}. He was born in ${yearOfBirth}. Today, he is ${calcAge(yearOfBirth)} years old.`);
